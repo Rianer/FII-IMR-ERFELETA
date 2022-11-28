663fd942bbf5d7b65b368ebeaccde933ad658d6c
Rianer <aerhan2001@gmail.com>
2022-11-28 14:25:44 +0200

    Weightlift tracking
    Created a "Player body". For now it is just a cube, further improvement/changes required.
    The player body has 2 colliders that act as triggers: shoulder level and knee level. When an object that is tagged as Liftable enters the shoulder level collider, an exercise is being tracked. After the object reaches the lower, knee level collider, the exercise is considered to be completed and a method is being called to handle exercise completion.

---
+++ Gym Simulator Bro/Gym Simulator Bro/Assets/scripts/PlayerBody.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBody : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private Camera playerCamera;
+    [SerializeField] private BoxCollider shoulderLevelCollider;
+    [SerializeField] private BoxCollider kneesLevelCollider;
+    [SerializeField] List<float> heights; //first elemenent = center of shoulder level colider, 
+                                          // second element = center of knee level colider,
+                                          // third element = height of collider
+                                          // fourth element = height of player body
+    private Transform playerTransform;
+    private Transform cameraTransform;
+
+    private float globaShoulderColliderLimit;
+    private float globalKneeColliderLimit;
+
+    private bool objectLifted;
+    void Start()
+    {
+        playerTransform = gameObject.GetComponent<Transform>();
+        cameraTransform = playerCamera.GetComponent<Transform>();
+        globaShoulderColliderLimit = heights[3]/2 + heights[0] - heights[2];
+        globalKneeColliderLimit = heights[3]/2 + heights[1] + heights[2];
+        objectLifted = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerTransform.position = new Vector3(cameraTransform.position.x, playerTransform.position.y, cameraTransform.position.z);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Liftable"))
+        {
+            Transform weightTransform = other.GetComponent<Transform>();
+            GroundDistance distance = other.GetComponent<GroundDistance>();
+
+            if (!objectLifted)
+            {
+                if (distance.distance >= globaShoulderColliderLimit)
+                {
+                    objectLifted = true;
+                }
+            }
+            else
+            {
+                if(distance.distance <= globalKneeColliderLimit)
+                {
+                    objectLifted = false;
+                    ApplyExerciseCompletion();
+                }
+            }
+        }
+
+    }
+
+
+    private void ApplyExerciseCompletion()
+    {
+        Debug.Log("Exercise is completed");
+    }
+}
